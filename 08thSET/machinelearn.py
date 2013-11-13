c076fe18fda3372ca892a3ba02751eff55675bbf
TAKENO SHUNSKE <takeno@jnlp.org>
2013-11-14 05:33:38 +0900

    First Commit for 8th SET

---
+++ 08thSET/machinelearn.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+#==============For Debugging===============
+_debug = True
+
+if _debug:
+	print "Running on debbugin mode"
+
+def dprint (str):
+	if __debug:
+		print "DEBUG:", str
+#=========================================#
+
+
+
+#=== main ===
+def main():
+
+	return
+
+if __name__ = "__main__":
+	main()
