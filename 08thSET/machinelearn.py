ff2c91be711cfa4f557581b4132cf1155c4acc08
TAKENO SHUNSKE <takeno@jnlp.org>
2013-11-14 07:09:04 +0900

    第8セット概要記述

--- 08thSET/machinelearn.py
+++ 08thSET/machinelearn.py
@@ -1,5 +1,21 @@
 # -*- coding:utf-8 -*-
 
+''' 第8セット内容:
+
+	二格のデータを用いて、
+	"前文脈の最後に出現する名詞句" と 
+	"後文脈の最初に出現する動詞句" を
+	素性として深層格の分類をする。
+
+	*1:?がつく深層格は?を除いた値を正しいものとして扱う
+	*2:深層格がx:不明が付されているものに関しては試験,
+	   学習データから除外
+'''
+
+#=== Import modules
+import sys
+import cabochamod as cmod
+
 
 #==============For Debugging===============
 _debug = True
@@ -11,6 +27,11 @@ def dprint (str):
 	if __debug:
 		print "DEBUG:", str
 #=========================================#
+
+# global variable(Configuration)
+path_datai	= "../Sample/work/"			#元データ
+lat_prectx	= "../Sample/work/prectx/"	#解析済み前文脈
+lat_pstctx	= "../Sample/work/postctx/"	#解析済み後文脈
 
 
 
