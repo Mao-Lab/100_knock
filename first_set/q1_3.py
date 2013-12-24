8b40a2a16b69ce97ec3d385216b7ee813a610d87
takahashi <takahashi@jnlp.org>
2013-12-24 17:22:52 +0900

    第1セット(3)修正

--- first_set/q1_3.py
+++ first_set/q1_3.py
@@ -3,24 +3,17 @@ import sys
 
 argvs = sys.argv
 
+fin  = open(argvs[1])
+fin1 = open('col1.txt','w')
+fin2 = open('col2.txt','w')
 
-f  = open(argvs[1])
-f1 = open('col1,txt','w')
-f2 = open('col2.txt','w')
-
-line = f.readline()
-col1 = []
-col2 = []
-i=0
+line = fin.readline()
 while line:
-    col1.append(line.split('\t')[0])
-    col2.append(line.split('\t')[1])
-    f1.writelines(col1[i]+"\n")
-    f2.writelines(col2[i])
-    i=i+1
-    line = f.readline()
+    fin1.writelines(line.split('\t')[0]+'\n')
+    fin2.writelines(line.split('\t')[1])
+    line = fin.readline()
 
-f.close
-f1.close
-f2.close
+fin.close()
+fin1.close()
+fin2.close()
 
