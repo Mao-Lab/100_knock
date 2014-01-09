b7ee3313a590ed5dd5167119b16064a33684e6cb
takahashi <takahashi@jnlp.org>
2014-01-09 14:40:14 +0900

    変なコメント訂正しました

--- first_set/q1_9.py
+++ first_set/q1_9.py
@@ -10,10 +10,7 @@ while line: #リストをタブで区切りながら読み込み
     line = fin.readline()
 fin.close()
 
-#リストの2つめの要素でソート
+#リストの2カラム目、1カラム目の順でソート
 for x, y in sorted(list, key = lambda x:(x[0],x[1]), reverse = True):
     print x + '\t' + y,
 
-
-#コマンドでの確認
-# sort -t "       " -k 2 address.txt > 1_8_2.csv
