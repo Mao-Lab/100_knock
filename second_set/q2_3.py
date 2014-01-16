5b4a2990c422f504dfc4bef3df76d32b87fb5f5a
takahashi <takahashi@jnlp.org>
2014-01-16 11:09:01 +0900

    第2セット(13)プログラム作成

---
+++ second_set/q2_3.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+#第2セット(13)非公式RTのツイートの中で、RT先へのコメント部分のみを抽出せよ
+import sys
+
+#自分のtwitterのつぶやきを１列に並べたものを利用
+fin = open('mytweet.txt', 'r')
+
+tl = list()
+line = fin.readline()
+while line:
+    tl.append(line)
+    line = fin.readline()
+
+fin.close()
+
+#非公式RTかどうかを判断、非公式RTにはRTの前にコメントが含まれると仮定
+for x in range(len(tl)):
+    tl[x] = tl[x].rstrip()
+    if(tl[x].startswith('RT') and tl[x].split('RT')[0]!=''):
+        print tl[x].split('RT')[1]
+
