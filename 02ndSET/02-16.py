dd0002f00998f30e815015d1671e9e8b5833c2e7
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-01-28 18:45:13 +0900

    16本目 括弧表現のうち，括弧の内側がアルファベット大文字，括弧の左側が漢字の文字列を抽出せよ．

---
+++ 02ndSET/02-16.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python 
+# -*- coding:utf-8 -*-
+
+import re
+
+tweetsCSV = 'tweets/tweets.csv'
+
+regx =  u'([一-龠]+)\(([A-Z]+)\)'
+
+def main():
+	with open(tweetsCSV) as fin:
+		fin.readline()
+
+		for line in fin:
+			cols = line.split(",")
+			if len(cols) <  6 : continue
+			tweet = cols[5]
+			
+			matches = re.finditer(regx, tweet.decode('utf-8'))
+			for matchobj in matches:
+				print  matchobj.group(1).encode('utf-8'), matchobj.group(2).encode('utf-8')
+
+			
+if __name__ == '__main__':
+	main()
+ 
