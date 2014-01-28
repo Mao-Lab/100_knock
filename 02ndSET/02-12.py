09166d56ab95c5dcfe9e08f7149de43a8a4d1760
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-01-28 14:20:53 +0900

    12本目 なうで終わるツイートを抽出

---
+++ 02ndSET/02-12.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python	
+# -*- coding:utf-8 -*-	
+
+tweetsCSV = 'tweets/tweets.csv'
+
+def main():
+ 	with open(tweetsCSV) as fin:
+		fin.readline()
+
+		for line in fin:
+			cols = line.split(",")
+			if len(cols) <  6 : continue
+			if cols[5].endswith("なう") :
+				print cols[5]
+
+	
+
+
+if __name__ == '__main__':
+	main()
