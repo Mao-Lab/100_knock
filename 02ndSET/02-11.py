c2ed5cca666b924a4ead013b07fc7e5542de1578
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-01-28 14:14:24 +0900

    11本目 拡散希望が含まれてる文字列を出力

---
+++ 02ndSET/02-11.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python 
+# -*- coding:utf-8 -*-
+
+tweetsCSV = 'tweets/tweets.csv'
+
+def main():
+	with open(tweetsCSV) as fin:
+		fin.readline()
+
+		for line in fin:
+			cols = line.split(",")
+			if len(cols) <  6 : continue
+			if("拡散希望" in cols[5]): 
+					print(cols[5]) 
+
+
+if __name__ == '__main__':
+	main()
+
