866c485396ef847c0ce7673fc76badf238d96bdc
vagrant <vagrant@devel.(none)>
2014-01-29 23:52:55 +0900

    18本目. 仙台市らしき住所の抽出

---
+++ 02ndSET/02-18.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python  
+# -*- coding:utf-8 -*- 
+
+ #! /usr/bin/python 
+# -*- coding:utf-8 -*-
+
+import re
+
+tweetsCSV = 'tweets/tweets.csv'
+
+regx =  u'(宮城県)?(仙台市)?.+区'
+
+def main():
+	with open(tweetsCSV) as fin:
+		fin.readline()
+
+		for line in fin:
+			cols = line.split(",")
+			if len(cols) <  6 : continue
+			tweet = cols[5]
+
+			matches = re.finditer(regx, tweet.decode('utf-8'))
+			for matchobj in matches:
+				print  matchobj.group(0).encode('utf-8')
+
+
+if __name__ == '__main__':
+	main()
+ 
+
