20242d02ee4605c5f11ab953305f8b91d7e2fd9a
vagrant <vagrant@devel.(none)>
2014-02-05 07:34:33 +0900

    22本目; ピリオド⇒スペース⇒大文字区切りで出力

---
+++ 03rdSET/03-22.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env Python 	
+# -*- coding:utf-8 -*- 
+
+import re
+
+path = './movie_reviews.txt'
+
+def main():
+	with open(path) as fin:
+		for line in fin:
+			print re.sub( r"\s\.([A-Z])", r' .\n\1', line.strip())
+
+if __name__ == '__main__':
+	main()
