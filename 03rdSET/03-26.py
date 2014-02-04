91b4ed1603bd694239101ce5133a2c8fe824daf8
vagrant <vagrant@devel.(none)>
2014-02-05 08:04:01 +0900

    26本目; ness か lyで終わる単語抽出

---
+++ 03rdSET/03-26.py
@@ -0,0 +1,19 @@
+ # !/usr/bin/env Python 	
+# -*- coding:utf-8 -*- 
+
+import re
+
+path = './movie_reviews.txt'
+
+def main():
+	with open(path) as fin:
+		for line in fin:
+			line = re.sub(r'\W+',' ',line.strip())
+			words = line.strip().split(' ')
+			for word in words:
+				if word.endswith('ness') or word.endswith('ly'):
+					print word 
+    
+
+if __name__ == '__main__':
+	main()
