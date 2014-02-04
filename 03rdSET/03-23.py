91a203a0ba790566b16928b97d192514e4b8e04c
vagrant <vagrant@devel.(none)>
2014-02-05 07:52:19 +0900

    23-24本目; 単語列に分割

---
+++ 03rdSET/03-23.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env Python 	
+# -*- coding:utf-8 -*- 
+
+import re
+
+path = './movie_reviews.txt'
+
+def main():
+	with open(path) as fin:
+		for line in fin:
+			line = re.sub(r'\W+',' ',line.strip())
+			print line.strip().replace(' ','\n')
+			print
+
+
+if __name__ == '__main__':
+	main()
