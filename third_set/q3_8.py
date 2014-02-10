f2c75a1fc15d039df5ab824c4ae75a1c055619dc
takahashi <takahashi@jnlp.org>
2014-02-10 15:06:03 +0900

    第3セット8本目プログラム修正。間違えて単語バイグラムにしていたのを、文字バイグラムに変更。処理時間が2秒ほどかかるのですが、こんなもんですか？バイグラムを取る単語の数は606419語です。

--- third_set/q3_8.py
+++ third_set/q3_8.py
@@ -1,37 +1,29 @@
 # -*- coding: utf-8 -*-
-#第3セット(27) (10)のプログラムを呼び出すことで、頻度の高い英単語トップ100(単語と頻度がソートされたもの)を作成せよ
+#第3セット(28) 各単語から文字バイグラムを抽出するプログラムを実装せよ．また，(27)と同様の方法で，頻度の高い文字バイグラムトップ100（バイグラムと頻度がソートされたもの）を作成せよ．
 from collections import defaultdict
 import sys
 import re
 #1行1単語形式のテキストを作った movie_reviews.txt.tok
 fin = open('movie_reviews.txt.tok', 'r')
 
-
-word2freq = defaultdict(int)
-#ファイルを一行ずつ読み込み
-head = 0
-tail = 0
+#word2bigram2freq（辞書型でバイグラム文字列と頻度を記録）
+word2bigram2freq = defaultdict(int)
+#バイグラム保持用変数
 bigram = ['', '']
-for word in fin:
-    word = word.rstrip('\n')
-    bigram[1] = bigram[0]
-    bigram[0] = word
-    bgmkey = bigram[1] + '\t' + bigram[0]
-
-    if bigram[1] == '':
-        head = 1
-    else:
-        head = 0
-
-    if bigram[0] == '':
-        tail = 1
-    else:
-        tail = 0
-
-    if head == 0 and tail == 0:
-#        print bgmkey
-        word2freq[bgmkey] += 1
+#ファイルを一行ずつ読み込み処理
+for words in fin:
+    words = words.rstrip('\n')
+    #リストの大きさの取得を一回にすることで早い？（何回もlen(words)と書くよりも）
+    wordslen = len(words)
+    #1文字はバイグラムに含めない
+    if wordslen == 1:
+        continue
+    #バイグラムの取得
+    for elem_num in range(0, wordslen):
+        if elem_num != 0 and elem_num != wordslen:
+            word2bigram2freq[words[elem_num-1] + words[elem_num]] += 1
 fin.close()
 
-for k, v in sorted(word2freq.items(), key = lambda x:x[1], reverse = True)[:100]:
+#頻度が大きい順にソートし、上位100件を表示
+for k, v in sorted(word2bigram2freq.items(), key = lambda x:x[1], reverse = True)[:100]:
     print "%s\t%d" %(k, v)
